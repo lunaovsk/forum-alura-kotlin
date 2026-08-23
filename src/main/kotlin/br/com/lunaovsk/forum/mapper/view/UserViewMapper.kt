2a3bced0dae6784aa22082bd134a56000e4ed895
lunaovsk <lunaovsk@users.noreply.github.com>
2026-08-23 14:38:21 -0300

    refactor: move UserViewMapper para pacote view

--- src/main/kotlin/br/com/lunaovsk/forum/mapper/view/UserViewMapper.kt
+++ src/main/kotlin/br/com/lunaovsk/forum/mapper/view/UserViewMapper.kt
@@ -1,4 +1,4 @@
-package br.com.lunaovsk.forum.mapper
+package br.com.lunaovsk.forum.mapper.view
 
 import br.com.lunaovsk.forum.dto.view.UserView
 import br.com.lunaovsk.forum.model.user.UserData
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component
 class UserViewMapper: Mapper<UserData, UserView> {
     override fun map(model: UserData): UserView {
         return UserView(
+            id = model.id,
             name = model.name,
             username = model.username
         )
