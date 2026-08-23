3705243ff27fac34da5a65587b1467c5766f4800
lunaovsk <lunaovsk@users.noreply.github.com>
2026-08-23 14:38:05 -0300

    feat: adiciona anotacoes de validacao no TopicRequest

--- src/main/kotlin/br/com/lunaovsk/forum/dto/request/TopicRequest.kt
+++ src/main/kotlin/br/com/lunaovsk/forum/dto/request/TopicRequest.kt
@@ -1,8 +1,15 @@
 package br.com.lunaovsk.forum.dto.request
 
+import jakarta.validation.constraints.NotBlank
+import org.jetbrains.annotations.NotNull
+
 data class TopicRequest(
-    var title: String,
-    var message: String,
-    var course: CourseRequest,
-    var user: UserRequest,
-    )
+    @NotBlank
+    val title: String,
+    @NotBlank
+    val message: String,
+    @NotNull
+    val userId: Long,
+    @NotNull
+    val courseId: Long,
+)
