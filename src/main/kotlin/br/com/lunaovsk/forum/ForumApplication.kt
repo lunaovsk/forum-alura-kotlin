63288284ce31a0ef1831e4306f8453d642e7148d
lunaovsk <lunaovsk@users.noreply.github.com>
2026-08-24 12:51:07 -0300

    config: habilita o gerenciamento de cache na aplicacao

--- src/main/kotlin/br/com/lunaovsk/forum/ForumApplication.kt
+++ src/main/kotlin/br/com/lunaovsk/forum/ForumApplication.kt
@@ -2,8 +2,12 @@ package br.com.lunaovsk.forum
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.web.config.EnableSpringDataWebSupport
 
 @SpringBootApplication
+@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 class ForumApplication
 
 fun main(args: Array<String>) {
