7259180396b64ab2ca90fbd981b1ec62aacf5b5c
lunaovsk <lunaovsk@users.noreply.github.com>
2026-08-24 12:51:07 -0300

    refactor: implementa Serializable em TopicView para o redis

--- src/main/kotlin/br/com/lunaovsk/forum/dto/view/TopicView.kt
+++ src/main/kotlin/br/com/lunaovsk/forum/dto/view/TopicView.kt
@@ -1,6 +1,7 @@
 package br.com.lunaovsk.forum.dto.view
 
 import br.com.lunaovsk.forum.model.topics.StatusTopic
+import java.io.Serializable
 import java.time.LocalDateTime
 
 data class TopicView (
@@ -11,4 +12,4 @@ data class TopicView (
     var status: StatusTopic,
     var answer: List<AnswerView>
 
-)
+)  : Serializable
